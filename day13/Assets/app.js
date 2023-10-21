4ce060b7afdbabdae973a81ef689bd5bb21e3d86
Misbahkanwal06 <125421589+Misbahkanwal06@users.noreply.github.com>
2023-10-21 00:31:37 -0700

    Add files via upload

---
+++ day13/Assets/app.js
@@ -0,0 +1,112 @@
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// // filter search for a value and return new array
+
+// let a = array.filter((num) => num > 5 ? num : null);
+// //  num % 2 == 0 ? "even" : "odd")
+// console.log(a);
+
+// let b = array.filter((num) => num % 2 == 0 ? num : null);
+// console.log(b);
+
+
+
+
+// // let c = array.filter((num)=> num % 2 == 0 ? "even" : "odd") ;
+// // console.log(c);
+// let objs = [
+//     {
+//         names: "misbah",
+//         roll: 31,
+//     },
+//     {
+//         names: "fasiha",
+//         roll: 12,
+//     },
+//     {
+//         names: "namrita",
+//         roll: 6,
+//     },
+//     {
+//         names: "sania",
+//         roll: 13,
+//     },
+// ];
+// let o = objs.filter((r) => r.roll % 2 == 0 ? r : null);
+// console.log(o);
+
+
+
+
+// // Find return only first record
+
+// let obj = [
+//     {
+//         names: "misbah",
+//         roll: 31,
+//     },
+//     {
+//         names: "fasiha",
+//         roll: 31,
+//     },
+//     {
+//         names: "namrita",
+//         roll: 6,
+//     },
+//     {
+//         names: "sania",
+//         roll: 13,
+//     },
+// ];
+// let d = obj.find((num) => num.roll == 31 ? num : num);
+// console.log(d);
+
+
+
+
+
+
+
+
+
+//   Find index 
+
+// let arrOfObj = [
+//     {
+//         naam: "cosmetics",
+//         stock: "5",
+//         title: "fasewash",
+//         price: "500"
+//     },
+//     {
+//         naam: "cosmetics",
+//         stock: "200",
+//         title: "cleanser",
+//         price: "600"
+//     },
+//     {
+//         naam: "cosmetics",
+//         stock: "10",
+//         title: "serum",
+//         price: "400"
+//     },
+// ];
+// let i = arrOfObj.find((n) => n.naam == "cosmetics" && n.stock == "10" ? n : null);
+// let i2 = arrOfObj.find((n) => n.naam == "cosmetics" || n.stock == "10" ? n : null);
+
+// console.log(i2);
+
+
+
+// CallBacks
+
+
+
+
+
+
+
+
+
+
+// Frame Work 
+// beppal interpreter converts js syntex in required version syntex 
